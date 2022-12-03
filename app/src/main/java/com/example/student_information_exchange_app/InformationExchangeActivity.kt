f8d03de65cf532f8a71c9c09e3ab1554dfe854c5
Bhumika shrestha <BhumikaShrestha@MacBook-Pro.lan>
2022-12-03 04:19:21 -0600

    Worked on news

--- app/src/main/java/com/example/student_information_exchange_app/InformationExchangeActivity.kt
+++ app/src/main/java/com/example/student_information_exchange_app/InformationExchangeActivity.kt
@@ -1,11 +1,30 @@
 package com.example.student_information_exchange_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class InformationExchangeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_information_exchange)
+        configurehomeScreenInfoEx()
+        configureNews()
     }
 }
+private fun configurehomeScreenInfoEx(){
+    val BackHomeInfoex: Button = findViewById(R.id.home_screen_infoExchange_button_btn)
+    BackHomeInfoex.setOnClickListener {
+        val backpage = Intent(this, HomePageActivity::class.java)
+        startActivity(backpage)
+    }
+}
+private fun configureNews(){
+    val News_Bttn: Button = findViewById(R.id.news_button)
+    News_Bttn.setOnClickListener {
+        val newsPage = Intent(this, NewsActivity::class.java)
+        startActivity(newsPage)
+    }
+}
+
