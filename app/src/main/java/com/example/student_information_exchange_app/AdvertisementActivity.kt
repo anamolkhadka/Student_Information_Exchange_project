ff635efd7966e0012f352c79fa972203b38c4790
iamAnamol <khadkaanamol8@gmail.com>
2022-12-06 13:48:35 -0600

    Completed Information and advertisement

---
+++ app/src/main/java/com/example/student_information_exchange_app/AdvertisementActivity.kt
@@ -0,0 +1,29 @@
+package com.example.student_information_exchange_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class AdvertisementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_advertisement)
+
+        configureSubmitButton()
+
+    }
+
+
+    private fun configureSubmitButton(){
+
+        val button: Button = findViewById(R.id.submit_advertising_form)
+        button.setOnClickListener{
+            Toast.makeText(applicationContext,"Your request has been submitted. Please wait for 5days for hearing back from us.", Toast.LENGTH_LONG).show()
+            val nextPage = Intent(this,HomePageDashboardActivity::class.java)
+            startActivity(nextPage)
+        }
+
+    }
+}
